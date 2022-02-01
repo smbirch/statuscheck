8f757b256bc6d7e1e4c904d278e55888e943c86e
smbirch <spencer.m.birch@gmail.com>
2022-02-01 14:00:56 -0800

    AddLink() created: can now add URLs to list

--- cmd/addlink.go
+++ cmd/addlink.go
@@ -1,7 +1,3 @@
-/*
-Copyright © 2022 NAME HERE <EMAIL ADDRESS>
-
-*/
 package cmd
 
 import (
@@ -18,7 +14,6 @@ var addlinkCmd = &cobra.Command{
 	Long:  ``,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("addlink called")
-		// fmt.Println("Addning %s to list", )
 		links.AddLink()
 
 	},
@@ -27,13 +22,4 @@ var addlinkCmd = &cobra.Command{
 func init() {
 	rootCmd.AddCommand(addlinkCmd)
 
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// addlinkCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// addlinkCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
