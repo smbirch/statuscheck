53865c4128a9d380a6a33b74eb7da48a4abcebb4
smbirch <spencer.m.birch@gmail.com>
2022-02-01 14:04:02 -0800

    cleaning up comments

--- cmd/root.go
+++ cmd/root.go
@@ -11,13 +11,8 @@ var rootCmd = &cobra.Command{
 	Use:   "statuscheck",
 	Short: "Are your favorite sites up?",
 	Long:  `statuscheck will check the status of your favorite websites!`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
 }
 
-// Execute adds all child commands to the root command and sets flags appropriately.
-// This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
 	err := rootCmd.Execute()
 	if err != nil {
@@ -26,13 +21,5 @@ func Execute() {
 }
 
 func init() {
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
-	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.statuscheck.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
